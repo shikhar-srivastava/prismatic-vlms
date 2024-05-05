83a6ee390ad2ea57e81f7664a1d8b286f87a41c5
shikhar-srivastava <shikharsrivastav@gmail.com>
2024-05-05 11:27:33 -0400

    update

--- continual.sh
+++ continual.sh
@@ -1,3 +1,23 @@
 # BASIC STAGE 0
 
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True OMP_NUM_THREADS=12 torchrun --standalone --nnodes 1 --nproc-per-node 4 scripts/pretrain.py   --stage "finetune" --model.type "stage0-after-llava+7b"  --dataset.type "llava-v1" --pretrained_checkpoint "/localdisk/ssrivas9/prismatic-vlms/runs/reproduction-llava-v15+7b+stage-align+x7/checkpoints/latest-checkpoint.pt" --run_id "stage-0-after-llava-158k"
+#PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True OMP_NUM_THREADS=12 torchrun --standalone --nnodes 1 --nproc-per-node 4 scripts/pretrain.py   --stage "finetune" --model.type "stage0-after-llava+7b"  --dataset.type "llava-v1" --pretrained_checkpoint "/localdisk/ssrivas9/prismatic-vlms/runs/reproduction-llava-v15+7b+stage-align+x7/checkpoints/latest-checkpoint.pt" --run_id "stage-0-after-llava-158k"
+
+# LoRA
+
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True OMP_NUM_THREADS=12 torchrun --standalone --nnodes 1 --nproc-per-node 4 scripts/pretrain.py   \
+--stage "finetune" --model.type "stage0-after-llava+7b"  --dataset.type "llava-v1" \
+--pretrained_checkpoint "/localdisk/ssrivas9/prismatic-vlms/runs/reproduction-llava-v15+7b+stage-align+x7/checkpoints/latest-checkpoint.pt" \
+--run_id "lora-stage-0-after-llava-158k" --mitigation "lora"
+
+
+# TEST
+
+# PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True OMP_NUM_THREADS=12 torchrun --standalone --nnodes 1 --nproc-per-node 4 scripts/pretrain.py   \
+# --stage "finetune" --model.type "stage0-after-llava+7b"  --dataset.type "llava-v1" \
+# --pretrained_checkpoint "/localdisk/ssrivas9/prismatic-vlms/runs/reproduction-llava-v15+7b+stage-align+x7/checkpoints/latest-checkpoint.pt" \
+# --run_id "lora-stage-0-after-llava-158k" --mitigation "lora"
+
+# PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True OMP_NUM_THREADS=12 torchrun --standalone --nnodes 1 --nproc-per-node 1 scripts/pretrain.py   \
+# --stage "finetune" --model.type "stage0-after-llava+7b"  --dataset.type "test" \
+# --pretrained_checkpoint "/localdisk/ssrivas9/prismatic-vlms/runs/reproduction-llava-v15+7b+stage-align+x7/checkpoints/latest-checkpoint.pt" \
+# --run_id "lora-stage-0-after-llava-158k" --mitigation "lora"
