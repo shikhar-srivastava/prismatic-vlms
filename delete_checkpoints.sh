e0f5d2d9ac9fce7528340b73bc5f187362ad75b0
shikhar-srivastava <shikharsrivastav@gmail.com>
2024-05-17 20:05:25 -0400

    update

--- delete_checkpoints.sh
+++
@@ -1,2 +0,0 @@
-find /scratch/ssrivas9/prismatic-vlms/runs/ -type d -name "checkpoints_base_llm" -exec rm -r {} +
-find /scratch/ssrivas9/prismatic-vlms/runs/ -type d -name "checkpoint_llm_only" -exec rm -r {} +
