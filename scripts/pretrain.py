d8ddb47cf44e49a751b70eb1eaab0eed9e18ace2
shikhar-srivastava <shikharsrivastav@gmail.com>
2024-05-18 16:09:24 -0400

    update

--- scripts/pretrain.py
+++ scripts/pretrain.py
@@ -115,9 +115,9 @@ class PretrainConfig:
             if self.bigger_batch is True:
                 self.global_batch_size = self.model.align_global_batch_size * 2 # 128
                 self.per_device_batch_size = self.model.finetune_per_device_batch_size # 16 (with 2 gradient accumulations) = 32
-            # if self.soft_alpha is not None:
-            #     self.global_batch_size = int(self.global_batch_size/2)
-            #     self.per_device_batch_size = int(self.per_device_batch_size/2)
+            if self.soft_alpha is not None:
+                self.global_batch_size = int(self.global_batch_size/2)
+                self.per_device_batch_size = int(self.per_device_batch_size/2)
             elif self.mitigation =='qlora':
                 self.global_batch_size = int(self.global_batch_size/2)
                 self.per_device_batch_size = int(self.per_device_batch_size/2)
