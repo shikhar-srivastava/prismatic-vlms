63145a21d0206a02d86900241dd3178b3e3aca90
shikhar-srivastava <shikharsrivastav@gmail.com>
2024-07-14 15:31:35 -0400

    Added new viz notebooks

--- parse_std.py
+++ parse_std.py
@@ -30,26 +30,31 @@ for model_name in os.listdir(root_dir):
                     metrics = json.load(f)
                     summary = metrics.get('summary', {})
                     
-                    # Capture accuracies based on the dataset
+                    # Capture accuracies and standard errors based on the dataset
                     if dataset == 'vqa-v2':
                         accuracy = summary.get('accuracy')
-                        if accuracy is not None:
-                            model_result['vqa-v2'] = accuracy / 100.0
+                        accuracy_stderr = summary.get('accuracy_stderr')
+                        if accuracy is not None and accuracy_stderr is not None:
+                            model_result['vqa-v2'] = (accuracy / 100.0, accuracy_stderr / 100.0)
                     elif dataset == 'text-vqa':
                         ocr_accuracy = summary.get('accuracy__TextVQA-OCR')
+                        ocr_accuracy_stderr = summary.get('accuracy_stderr__TextVQA-OCR')
                         pure_accuracy = summary.get('accuracy__TextVQA-Pure')
-                        if ocr_accuracy is not None:
-                            model_result['textvqa-ocr'] = ocr_accuracy
-                        if pure_accuracy is not None:
-                            model_result['textvqa-pure'] = pure_accuracy
+                        pure_accuracy_stderr = summary.get('accuracy_stderr__TextVQA-Pure')
+                        if ocr_accuracy is not None and ocr_accuracy_stderr is not None:
+                            model_result['textvqa-ocr'] = (ocr_accuracy, ocr_accuracy_stderr)
+                        if pure_accuracy is not None and pure_accuracy_stderr is not None:
+                            model_result['textvqa-pure'] = (pure_accuracy, pure_accuracy_stderr)
                     elif dataset == 'gqa':
                         accuracy = summary.get('accuracy')
-                        if accuracy is not None:
-                            model_result['gqa'] = accuracy / 100.0
+                        accuracy_stderr = summary.get('accuracy_stderr')
+                        if accuracy is not None and accuracy_stderr is not None:
+                            model_result['gqa'] = (accuracy / 100.0, accuracy_stderr / 100.0)
                     elif dataset == 'refcoco':
                         accuracy = summary.get('accuracy__RefCOCO')
-                        if accuracy is not None:
-                            model_result['refcoco'] = accuracy
+                        accuracy_stderr = summary.get('accuracy_stderr__RefCOCO')
+                        if accuracy is not None and accuracy_stderr is not None:
+                            model_result['refcoco'] = (accuracy, accuracy_stderr)
 
         # Parse NLU/NLG results
         nlu_path = os.path.join(model_path, 'nlp/nlu', 'results.json')
@@ -59,8 +64,9 @@ for model_name in os.listdir(root_dir):
                 for nlu_dataset in nlu_datasets:
                     if nlu_dataset in nlu_results:
                         acc = nlu_results[nlu_dataset].get('acc,none')
-                        if acc is not None:
-                            model_result[nlu_dataset] = acc
+                        acc_stderr = nlu_results[nlu_dataset].get('acc_stderr,none')
+                        if acc is not None and acc_stderr is not None:
+                            model_result[nlu_dataset] = (acc, acc_stderr)
 
         # Parse triviaqa results
         triviaqa_path = os.path.join(model_path, 'nlp/triviaqa', 'results.json')
@@ -69,8 +75,9 @@ for model_name in os.listdir(root_dir):
                 triviaqa_results = json.load(f).get('results', {})
                 if 'triviaqa' in triviaqa_results:
                     exact_match = triviaqa_results['triviaqa'].get('exact_match,remove_whitespace')
-                    if exact_match is not None:
-                        model_result['triviaqa'] = exact_match
+                    exact_match_stderr = triviaqa_results['triviaqa'].get('exact_match_stderr,remove_whitespace')
+                    if exact_match is not None and exact_match_stderr is not None:
+                        model_result['triviaqa'] = (exact_match, exact_match_stderr)
 
         # Parse webqs results
         webqs_path = os.path.join(model_path, 'nlp/webqs', 'results.json')
@@ -79,15 +86,16 @@ for model_name in os.listdir(root_dir):
                 webqs_results = json.load(f).get('results', {})
                 if 'webqs' in webqs_results:
                     exact_match = webqs_results['webqs'].get('exact_match,none')
-                    if exact_match is not None:
-                        model_result['webqs'] = exact_match
+                    exact_match_stderr = webqs_results['webqs'].get('exact_match_stderr,none')
+                    if exact_match is not None and exact_match_stderr is not None:
+                        model_result['webqs'] = (exact_match, exact_match_stderr)
 
         # Add the model results to the final result dictionary
         if model_result:
             result[model_name] = model_result
 
 # Save the final JSON to results_nlp.json
-with open('results_nlp.json', 'w') as f:
+with open('results_std.json', 'w') as f:
     json.dump(result, f, indent=2)
 
-print(f"Results saved to results_nlp.json")
+print(f"Results saved to results_std.json")
